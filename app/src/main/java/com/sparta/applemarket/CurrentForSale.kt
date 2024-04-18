9691cb3d6da2f09edd7e74fdfbe1df477e6538fc
Sooyoung <agriades23@gmail.com>
2024-04-18 12:06:21 +0900

    [feat] CurrentForSale에 더미데이터를 집어넣다가, 긴 String은 아예 res로 빼는 게 낫겠다는 생각을 함.

--- app/src/main/java/com/sparta/applemarket/CurrentForSale.kt
+++ app/src/main/java/com/sparta/applemarket/CurrentForSale.kt
@@ -1,4 +1,8 @@
 package com.sparta.applemarket
 
 class CurrentForSale {
+    val forSale1 = ForSale(1, "sample1", "산지 한 달 된 선풍기 팝니다",
+        "이사가서 필요가 없어졌어요 급하게 내놓습니다", "대현동", 1000, "서울 서대문구 창천동", 13, 25)
+    val forSale2 = ForSale(2, "sample2", "김치냉장고",
+        "이사로 인해 내놔요", "안마담", 20000, "인천 계양구 귤현동", 13, 25)
 }
