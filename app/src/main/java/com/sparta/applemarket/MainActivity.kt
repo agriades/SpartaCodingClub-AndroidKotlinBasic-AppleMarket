c79fac319e818c0bb32aa5613bd58686b7d44e8d
Sooyoung <agriades23@gmail.com>
2024-04-19 06:42:21 +0900

    [fix] 아참, 그 전에 layoutManager를 사용해서 어댑터로 받은 정보를 recyclerView 안에 뿌려주어야 한다. layoutManager의 종류는 4개 정도 있는데, 세로로 나열되는 지금과 같은 경우엔 LinearLayoutManager를 사용하면 된다.

--- app/src/main/java/com/sparta/applemarket/MainActivity.kt
+++ app/src/main/java/com/sparta/applemarket/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.sparta.applemarket
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sparta.applemarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +12,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val dataList = mutableListOf<ForSale>(CurrentForSale.forSale1)
+        val dataList = mutableListOf<ForSale>(CurrentForSale.forSale1, CurrentForSale.forSale2)
         val adapter = ForSaleAdapter(dataList)
         binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this) //세로로 연결되므로.
 
     }
 }
