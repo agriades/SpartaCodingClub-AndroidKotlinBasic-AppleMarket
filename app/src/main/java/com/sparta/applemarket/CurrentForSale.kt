5250ebb2a53955250d8e180d0e89b5ca01a40115
Sooyoung <agriades23@gmail.com>
2024-04-18 12:21:54 +0900

    [fix] CurrentForSale에 더미데이터를 모두 추가함.

--- app/src/main/java/com/sparta/applemarket/CurrentForSale.kt
+++ app/src/main/java/com/sparta/applemarket/CurrentForSale.kt
@@ -1,8 +1,26 @@
 package com.sparta.applemarket
 
 class CurrentForSale {
-    val forSale1 = ForSale(1, "sample1", "산지 한 달 된 선풍기 팝니다",
-        "이사가서 필요가 없어졌어요 급하게 내놓습니다", "대현동", 1000, "서울 서대문구 창천동", 13, 25)
-    val forSale2 = ForSale(2, "sample2", "김치냉장고",
-        "이사로 인해 내놔요", "안마담", 20000, "인천 계양구 귤현동", 13, 25)
+    val forSale1 = ForSale(1, "sample1", "@strings/fs_title_1", "@strings/fs_desc_1",
+        "@strings/fs_seller_1", 1000, "@strings/fs_address_1", 13, 25)
+    val forSale2 = ForSale(2, "sample2", "@strings/fs_title_2", "@strings/fs_desc_2",
+        "@strings/fs_seller_2", 20000, "@strings/fs_address_2", 8, 28)
+    val forSale3 = ForSale(3, "sample3", "@strings/fs_title_3", "@strings/fs_desc_3",
+        "@strings/fs_seller_3", 10000, "@strings/fs_address_3", 23, 5)
+    val forSale4 = ForSale(4, "sample4", "@strings/fs_title_4", "@strings/fs_desc_4",
+        "@strings/fs_seller_4", 10000, "@strings/fs_address_4", 14, 17)
+    val forSale5 = ForSale(5, "sample5", "@strings/fs_title_5", "@strings/fs_desc_5",
+        "@strings/fs_seller_5", 150000, "@strings/fs_address_5", 22, 9)
+    val forSale6 = ForSale(6, "sample6", "@strings/fs_title_6", "@strings/fs_desc_6",
+        "@strings/fs_seller_6", 50000, "@strings/fs_address_6", 25, 16)
+    val forSale7 = ForSale(7, "sample7", "@strings/fs_title_7", "@strings/fs_desc_7",
+        "@strings/fs_seller_7", 150000, "@strings/fs_address_7", 142, 54)
+    val forSale8 = ForSale(8, "sample8", "@strings/fs_title_8", "@strings/fs_desc_8",
+        "@strings/fs_seller_8", 180000, "@strings/fs_address_8", 31, 7)
+    val forSale9 = ForSale(9, "sample9", "@strings/fs_title_9", "@strings/fs_desc_9",
+        "@strings/fs_seller_9", 30000, "@strings/fs_address_9", 7, 28)
+    val forSale10 = ForSale(10, "sample10", "@strings/fs_title_10", "@strings/fs_desc_10",
+        "@strings/fs_seller_10", 190000, "@strings/fs_address_10", 40, 6)
+
+
 }
