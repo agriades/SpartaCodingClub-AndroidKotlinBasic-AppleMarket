6c44a632c9099328eb565f535dc0e613f9224524
Sooyoung <agriades23@gmail.com>
2024-04-18 12:45:06 +0900

    [feat] 뷰바인딩으로 MainActivity.kt에 activity_main.xml를 연결함.

--- app/src/main/java/com/sparta/applemarket/MainActivity.kt
+++ app/src/main/java/com/sparta/applemarket/MainActivity.kt
@@ -2,10 +2,13 @@ package com.sparta.applemarket
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sparta.applemarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
