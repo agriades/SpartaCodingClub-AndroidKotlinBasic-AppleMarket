c51c3b62da8e9a8ee5f62364869c02b826281eb9
Sooyoung <agriades23@gmail.com>
2024-04-19 07:45:25 +0900

    [fix] DetailActivity도 바인딩을 시켜줌. 변수명 binding으로 main이랑 겹쳐도 상관 없겠지?

--- app/src/main/java/com/sparta/applemarket/DetailActivity.kt
+++ app/src/main/java/com/sparta/applemarket/DetailActivity.kt
@@ -1,20 +1,15 @@
 package com.sparta.applemarket
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.sparta.applemarket.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
